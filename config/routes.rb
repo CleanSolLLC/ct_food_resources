3ff267c6e9a780039f5a7a775c5a41332cd56814
CleanSolLLC <mamark90@gmail.com>
2025-02-09 11:55:45 -0500

    Modify sessions new, add nav bar and tailwindcss styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/dashboard"
   resource :session
   resources :passwords, param: :token
+
+  root "pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
