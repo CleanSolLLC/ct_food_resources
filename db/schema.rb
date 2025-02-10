c8c7443595ee7718230c4f270fcd43b6b91c4c70
CleanSolLLC <mamark90@gmail.com>
2025-02-10 05:55:42 -0500

    Migrate files

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_024213) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_104701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "food_pantry_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_pantry_id"], name: "index_comments_on_food_pantry_id"
+  end
+
+  create_table "counties", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "food_pantries", force: :cascade do |t|
+    t.string "name"
+    t.string "address_line1"
+    t.string "address_line2"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "phone_number"
+    t.string "phone_ext"
+    t.string "contact"
+    t.boolean "verified"
+    t.integer "user_id"
+    t.bigint "town_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["town_id"], name: "index_food_pantries_on_town_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,6 +53,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_024213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "towns", force: :cascade do |t|
+    t.string "name"
+    t.bigint "county_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["county_id"], name: "index_towns_on_county_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +72,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_024213) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "comments", "food_pantries"
+  add_foreign_key "food_pantries", "towns"
   add_foreign_key "sessions", "users"
+  add_foreign_key "towns", "counties"
 end
